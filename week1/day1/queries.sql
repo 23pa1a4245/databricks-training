6f0a26309ed96c58234976b73cea37ff47e2e965
23pa1a4245 <23pa1a4245@vishnu.edu.in>
2026-05-06 22:42:29 +0530

    Create queries.sql

---
+++ week1/day1/queries.sql
@@ -0,0 +1,12 @@
+select * from Employee;
+
+select name, salary from Employee;
+
+select * from Employee
+where salary>50000;
+
+select*from Employee
+where department_id=1;
+
+select*from Employee
+where hire_date > '2020-01-01';
